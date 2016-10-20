6f8114f0c1c6a02cac3611cc5d8bbbf296e10f28
Kylie Le <kylie.tram.le@gmail.com>
2016-10-20 15:01:15 -0700

    search bar built, not filtering results

--- 99Beers/99Beers/SearchViewController.swift
+++ 99Beers/99Beers/SearchViewController.swift
@@ -12,11 +12,15 @@ import Alamofire
 let clientID = "32BEBC190F5DE4785EED12F6527239AF2623E77D"
 let clientKey = "76BFCCB912EAB5AB2FFC7672C55A5E9530F9492F"
 
-class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchResultsUpdating {
 
     @IBOutlet weak var searchResultTableView: UITableView!
     
     var beers: [Beer]? = []
+    var filteredBeers = [Beer]()
+    
+    var searchController: UISearchController!
+    var resultsController = UITableViewController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,18 +29,48 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
         searchResultTableView.dataSource = self
         
         makeAPICall()
+        
+        // look for search results in itself
+        self.resultsController.tableView.dataSource = self
+        self.resultsController.tableView.delegate = self
+        
+        // build search bar
+        self.searchController = UISearchController(searchResultsController: self.resultsController)
+        self.searchResultTableView.tableHeaderView = self.searchController.searchBar
+        self.searchController.searchResultsUpdater = self
+    }
+    
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        // filter through the beers
+            
+        self.beers?.filter({ (beer: Beer) -> Bool in
+            if beer.beerName!.containsString(self.searchController.searchBar.text!) {
+                return true
+            } else {
+                return false
+            }
+        })
+        // update the results TableView
+        self.searchResultTableView.reloadData()
+        
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       return beers?.count ?? 0
-        
+        if tableView == self.searchResultTableView {
+            return beers?.count ?? 0
+        } else {
+            return filteredBeers.count
+        }
     }
     
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = searchResultTableView.dequeueReusableCellWithIdentifier("BeerResultCell", forIndexPath: indexPath)
-        
-        cell.textLabel?.text = beers![indexPath.row].beerName
+        if tableView == self.searchResultTableView {
+            cell.textLabel?.text = beers![indexPath.row].beerName
+        } else {
+            cell.textLabel?.text = filteredBeers[indexPath.row].beerName
+        }
         
         return cell
         

c857fa28dedadedf7b72d78e7cb7160c5886451c
Kylie Le <kylie.tram.le@gmail.com>
2016-10-26 12:59:12 -0700

    new search bar method

--- 99Beers/99Beers/SearchViewController.swift
+++ 99Beers/99Beers/SearchViewController.swift
@@ -12,68 +12,70 @@ import Alamofire
 let clientID = "32BEBC190F5DE4785EED12F6527239AF2623E77D"
 let clientKey = "76BFCCB912EAB5AB2FFC7672C55A5E9530F9492F"
 
-class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchResultsUpdating {
+class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
 
     @IBOutlet weak var searchResultTableView: UITableView!
     
+    let searchBar = UISearchBar()
     var beers: [Beer]? = []
-    var filteredBeers = [Beer]()
     
-    var searchController: UISearchController!
-    var resultsController = UITableViewController()
+    // variables for search function
+    var filteredBeers = [Beer]()
+    var shouldShowSearchResults = false
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        searchResultTableView.delegate = self
-        searchResultTableView.dataSource = self
-        
+        createSearchBar()
         makeAPICall()
         
-        // look for search results in itself
-        self.resultsController.tableView.dataSource = self
-        self.resultsController.tableView.delegate = self
-        
-        // build search bar
-        self.searchController = UISearchController(searchResultsController: self.resultsController)
-        self.searchResultTableView.tableHeaderView = self.searchController.searchBar
-        self.searchController.searchResultsUpdater = self
+        searchResultTableView.delegate = self
+        searchResultTableView.dataSource = self
     }
     
-    func updateSearchResultsForSearchController(searchController: UISearchController) {
-        // filter through the beers
-            
-        self.filteredBeers = (self.beers?.filter({ (beer: Beer) -> Bool in
-            if beer.beerName!.containsString(self.searchController.searchBar.text!) {
-                return true
-            } else {
-                return false
-            }
-        }))!
-        // update the results TableView
-        self.resultsController.tableView.reloadData()
+    func createSearchBar() {
+        searchBar.showsCancelButton = false
+        searchBar.placeholder = "Anchor Steam"
+        searchBar.delegate = self
         
+        // put into navigation bar
+        self.navigationItem.titleView = searchBar
+    }
+    
+    // Functions for adding search
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        filteredBeers = (beers?.filter({ (beer: Beer) -> Bool in
+            return beer.beerName?.lowercaseString.rangeOfString(searchText.lowercaseString) != nil
+        }))!
+        
+        if searchText != "" {
+            shouldShowSearchResults = true
+            self.searchResultTableView.reloadData()
+        } else {
+            shouldShowSearchResults = false
+            self.searchResultTableView.reloadData()
+        }
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if tableView == self.searchResultTableView {
-            return beers?.count ?? 0
-        } else {
+        if shouldShowSearchResults {
             return filteredBeers.count
+        } else {
+            return beers!.count
         }
     }
     
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = searchResultTableView.dequeueReusableCellWithIdentifier("BeerResultCell", forIndexPath: indexPath)
-        if tableView == self.searchResultTableView {
-            cell.textLabel?.text = beers![indexPath.row].beerName
-        } else {
+        if shouldShowSearchResults {
             cell.textLabel?.text = filteredBeers[indexPath.row].beerName
+        } else {
+            cell.textLabel?.text = beers![indexPath.row].beerName
         }
         
         return cell
-        
     }
     
     func makeAPICall() {
@@ -110,6 +112,17 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
         let beerDetailsController = segue.destinationViewController as! BeerDetailsViewController
         
         beerDetailsController.beer = beer
+    }
+    
+    func scrollViewWillBeginDragging(scrollView: UIScrollView) {
+        searchBar.endEditing(true)
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        shouldShowSearchResults = true
+        searchBar.endEditing(true)
+        
+        self.searchResultTableView.reloadData()
     }
 
 }

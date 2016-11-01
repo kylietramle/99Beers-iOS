3ae3a501ffa7586fdfccaef2cdc3943d4fcc1b57
Kylie Le <kylie.tram.le@gmail.com>
2016-11-01 13:26:03 -0700

    search bar

--- 99Beers/99Beers/SearchViewController.swift
+++ 99Beers/99Beers/SearchViewController.swift
@@ -19,11 +19,6 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
     let searchBar = UISearchBar()
     var beers: [Beer]? = []
     
-    // variables for search function
-    var filteredBeers = [Beer]()
-    var shouldShowSearchResults = false
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -35,60 +30,45 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
     }
     // Functions for adding search
     func createSearchBar() {
-        searchBar.showsCancelButton = false
         searchBar.placeholder = "Anchor Steam"
         searchBar.delegate = self
         
         // put into navigation bar
         self.navigationItem.titleView = searchBar
     }
-
+    
     func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
         makeAPICall(searchBar.text!)
-        
-        shouldShowSearchResults = true
-        searchBar.endEditing(true)
-        
+    }
+    
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        self.searchBar.showsCancelButton = true
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
         self.searchResultTableView.reloadData()
     }
     
-    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
-        filteredBeers = (beers?.filter({ (beer: Beer) -> Bool in
-            return beer.beerName?.lowercaseString.rangeOfString(searchText.lowercaseString) != nil
-            
-        }))!
-        
-        if searchText != "" {
-            shouldShowSearchResults = true
-            self.searchResultTableView.reloadData()
-        } else {
-            shouldShowSearchResults = false
-            self.searchResultTableView.reloadData()
-        }
-    }
-
+    // End of search bars
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if shouldShowSearchResults {
-            return filteredBeers.count
-        } else {
-            return beers!.count
-        }
+       return beers!.count ?? 0
     }
     
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = searchResultTableView.dequeueReusableCellWithIdentifier("BeerResultCell", forIndexPath: indexPath)
-        if shouldShowSearchResults {
-            print("tableView here")
-            cell.textLabel?.text = filteredBeers[indexPath.row].beerName
-        } else {
-            cell.textLabel?.text = beers![indexPath.row].beerName
-        }
+        cell.textLabel?.text = beers![indexPath.row].beerName
         
         return cell
     }
     
     func makeAPICall(beer: String) {
+        print("THE SEARCHED BEER IS \(beer)")
          Alamofire.request(.GET, "https://api.untappd.com/v4/search/beer?q=\(beer)&client_id=32BEBC190F5DE4785EED12F6527239AF2623E77D&client_secret=76BFCCB912EAB5AB2FFC7672C55A5E9530F9492F").responseJSON { response in
                 if let json = response.result.value {
                     print ("Connection to API successful!")
@@ -102,7 +82,9 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
                                 }
                                 
                                 self.beers = Beer.convertBeers((beersJsonArray as? [NSDictionary]!)!)
-                                print(self.beers)
+//                                print(self.beers)
+                                self.searchResultTableView.reloadData()
+
                             }
                         }
                     }
@@ -110,6 +92,7 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
             
         }
     }
+        
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         
